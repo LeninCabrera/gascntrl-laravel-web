c05c24fb8627329985264519a44c2194fa8a52b2
JoelArtetaUrbina <45246279+JoelArtetaUrbina@users.noreply.github.com>
2022-02-15 21:01:06 -0600

    feat: adding migrations of category table
    -Add Migrations 'clientes'
    -Add Migrations 'dispensadors'
    -Add Migrations 'personals'
    -Add Migrations 'permisos'
    -Add Migrations 'tarjetas_creditos'
    -Add Migrations 'tasa_cambios'
    -Add Migrations 'tipo_cupons'

---
+++ src/gascontrol/database/migrations/2022_02_16_012324_create_tasa_cambios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasaCambiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasa_cambios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasa_cambios');
+    }
+}
