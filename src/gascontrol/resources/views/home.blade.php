148f0178883d44180bd8f2a9d0cd6d70a59253ae
LeninCabrera <lenincabrera57@gmail.com>
2022-02-23 14:58:10 -0600

    Feat: updating style for tailwind css.

--- src/gascontrol/resources/views/home.blade.php
+++ src/gascontrol/resources/views/home.blade.php
@@ -3,45 +3,45 @@
     <x-jet-breadcrumbs-nav>
     </x-jet-breadcrumbs-nav>
     <div class="h-screen mt-6 flex">
-        <div class="mx-auto flex-auto">
+        <div class="mx-auto">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <a href="{{ route('settings.module') }}">
                     <div class="bg-gray-200 flex justify-center items-center">
                         <div id="" class="flex items-center"> 
-                            <x-jet-icon.application-settings />
+                            <x-jet-icon.application-settings class="ease-in-out transform hover:scale-75 transition duration-500" />
                         </div>
                     </div>
                 </a>
             </div>
         </div>
-        <div class="mx-auto flex-auto">
+        <div class="mx-auto">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <a href="{{ route('settings.module') }}">
                     <div class="bg-gray-200 flex justify-center items-center">
                         <div id="" class="flex items-center"> 
-                            <x-jet-icon.application-dispenser />
+                            <x-jet-icon.application-dispenser class="ease-in-out transform hover:scale-75 transition duration-500" />
                         </div>
                     </div>
                 </a>
             </div>
         </div>
-        <div class="mx-auto flex-auto">
+        <div class="mx-auto">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <a href="{{ route('settings.module') }}">
                     <div class="bg-gray-200 flex justify-center items-center">
                         <div id="" class="flex items-center"> 
-                            <x-jet-icon.application-purchase />
+                            <x-jet-icon.application-purchase class="ease-in-out transform hover:scale-75 transition duration-500" />
                         </div>
                     </div>
                 </a>
             </div>
         </div>
-        <div class="mx-auto flex-auto">
+        <div class="mx-auto">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <a href="{{ route('dispenser.module') }}">
                     <div class="bg-gray-200 flex justify-center items-center">
                         <div id="" class="flex items-center"> 
-                            <x-jet-icon.application-product />
+                            <x-jet-icon.application-product class="ease-in-out transform hover:scale-75 transition duration-500" />
                         </div>
                     </div>
                 </a>
--- src/gascontrol/resources/views/livewire/dispenser-component.blade.php
+++ src/gascontrol/resources/views/livewire/dispenser-component.blade.php
@@ -8,12 +8,12 @@
                 </svg>
             </span>
         
-            <a href="{{ route('dispenser.module') }}" class="text-gray-600 dark:text-gray-200 hover:underline"> Dispensadores </a>
+            <a href="{{ route('dispenser.module') }}" class="text-gray-600 hover:underline"> Dispensadores </a>
         </x-slot>
     </x-jet-breadcrumbs-nav>
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="py-2 align-middle inline-block w-1/2 sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     <x-jet-datatable id="dispensersTable">
                         <x-slot name="thead">
