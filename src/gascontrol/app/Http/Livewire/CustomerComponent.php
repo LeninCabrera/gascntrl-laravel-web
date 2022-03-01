72d7b4305f17dc896066704571fee7b1fbaeac9d
JoelArtetaUrbina <45246279+JoelArtetaUrbina@users.noreply.github.com>
2022-02-28 20:53:29 -0600

    fix: adding style and best practices in Customer

--- src/gascontrol/app/Http/Livewire/CustomerComponent.php
+++ src/gascontrol/app/Http/Livewire/CustomerComponent.php
@@ -4,11 +4,17 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Customer;
+use App\Http\Livewire\ComponentContractInterface;
 
-class CustomerComponent extends Component
+class CustomerComponent extends Component implements ComponentContractInterface
 {
-    public $idCard, $name, $lastName, $address,
-    $cellphone, $email;
+    // Public Properties
+    public $name;
+    public $email;
+    public $idCard;
+    public $address;
+    public $lastName;
+    public $cellphone;
 
     public function render()
     {
@@ -17,14 +23,48 @@ class CustomerComponent extends Component
         ]);
     }
 
-    public function saveNewCustomer(){
+    public function resetFilters()
+    {
+        $properties = [
+            'name',
+            'email',
+            'idCard',
+            'address',
+            'lastName',
+            'cellphone'
+        ];
+        foreach ($properties as $property) {
+            $this->reset($property);
+        }
+    }
+
+    public function saveNewCustomer()
+    {
+        $this->saveModelObject();
+        $this->resetFilters();
+    }
+
+    // Public functions from ComponentContractInterface
+
+    public function saveModelObject()
+    {
         Customer::Create([
-            'id_Card' => $this->idCard,
-            'name' => $this->name,
-            'last_Name' => $this->lastName,
-            'cellphone' => $this->cellphone,
-            'address' => $this->address,
-            'email' => $this->email,
+            'name'          => $this->name,
+            'email'         => $this->email,
+            'address'       => $this->address,
+            'id_Card'       => $this->idCard,
+            'last_Name'     => $this->lastName,
+            'cellphone'     => $this->cellphone,
         ]);
     }
+
+    public function updateModelObjectById($objectId)
+     {
+         // TODO: se debe crear la acción de eliminar para el registro de este componente
+     }
+
+    public function deleteModelObjectById($objectId)
+     {
+         // TODO: se debe crear la acción de eliminar para el registro de este componente
+     }
 }
