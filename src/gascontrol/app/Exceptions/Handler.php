988224b828f563eb59fa942b71b99e8fb6019e35
LeninCabrera <lenincabrera57@gmail.com>
2022-03-24 14:38:11 -0600

    Fix: Report function in Exeptions Handler was removed.

--- src/gascontrol/app/Exceptions/Handler.php
+++ src/gascontrol/app/Exceptions/Handler.php
@@ -40,13 +40,4 @@ class Handler extends ExceptionHandler
             }
         });
     }
-
-    public function report(Throwable $exception)
-    {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
-        }
-
-        parent::report($exception);
-    }
 }
